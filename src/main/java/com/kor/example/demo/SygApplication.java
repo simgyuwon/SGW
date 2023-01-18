369f54a2287887604a92d7d333c27097092bd47c
simgyuwon <sim5034@naver.com>
2023-01-18 14:46:49 +0900

    firest project commit

---
+++ src/main/java/com/kor/example/demo/SygApplication.java
@@ -0,0 +1,13 @@
+package com.kor.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SygApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SygApplication.class, args);
+	}
+
+}
